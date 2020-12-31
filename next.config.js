aa00c0f256ca270a5dcb404607bcf4125120a42a
Steve Occhipinti <dev@stevenocchipinti.com>
2020-12-31 17:39:37 +1100

    init

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  webpack: (config, { isServer }) => {
+    // Fixes npm packages that depend on `fs` module
+    if (!isServer) config.node = { fs: "empty" }
+
+    return config
+  },
+}
