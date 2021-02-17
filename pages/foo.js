5b0dda29693bcc3b9a842a992d967a83c6f88a3b
Steve Occhipinti <dev@stevenocchipinti.com>
2021-02-17 20:24:07 +1100

    Working prototype of firebase + mobx + tree structure!

--- pages/foo.js
+++ pages/foo.js
@@ -5,44 +5,16 @@ import { ProjectStore } from "../lib/Tree"
 
 const ProjectContext = createContext()
 
-const Node = observer(({ node }) => {
-  console.log("render", node.title)
+const ListItem = observer(({ item }) => {
+  // console.log("render", item.title)
   return (
-    <div tw="border rounded p-4">
-      <h2>{node.title}</h2>
-      <ul>
-        {node.children.map(c => (
-          <Node key={c.id} node={c} />
-        ))}
-      </ul>
+    <div tw="border m-1 p-2">
+      {item.title}
+      {item.children.map(
+        child => child && <ListItem key={child.id} item={child} />
+      )}
     </div>
   )
-})
-
-const Project = observer(() => {
-  const project = useContext(ProjectContext)
-
-  if (!project || project.isLoading) return <h1>Loading...</h1>
-  return (
-    <main tw="m-4 p-4">
-      <h1>{project.name}</h1>
-      <Node node={project.root} />
-
-      <button
-        tw="my-4 p-4 rounded text-center bg-blue-500 text-white"
-        onClick={() => {
-          project.addTask()
-        }}
-      >
-        Add a new node
-      </button>
-    </main>
-  )
-})
-
-const ListItem = observer(({ item }) => {
-  console.log("render", item.id)
-  return <li tw="border m-1 p-2">{JSON.stringify(item.asJS, null, 2)}</li>
 })
 
 const List = observer(() => {
@@ -51,12 +23,15 @@ const List = observer(() => {
   return (
     <main tw="m-4 p-4">
       <h1>{project.name}</h1>
-      <ul>
-        {Object.keys(project.tree).map(id => (
-          <ListItem key={id} item={project.tree[id]} />
-        ))}
-        <li></li>
-      </ul>
+      {project.root && <ListItem item={project.root} />}
+      <button
+        tw="my-4 p-4 rounded text-center bg-blue-500 text-white"
+        onClick={() => {
+          project.addTask()
+        }}
+      >
+        Add a new node
+      </button>
     </main>
   )
 })
