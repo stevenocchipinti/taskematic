1d82be6fdbbf8153f17118af37e51ed0ac3d354c
Steve Occhipinti <dev@stevenocchipinti.com>
2021-03-22 17:43:29 +1100

    A first attempt at a realfavicon
    Generated from:
    https://realfavicongenerator.net/favicon_result?file_id=p1f1blfqfr1or110oj1i741h409956#.YFfnq0gza3I

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,7 @@
 // on load. Without it, the styles are only added once
 // react loads on the frontend
 
-import Document from "next/document"
+import Document, { Html, Head, Main, NextScript } from "next/document"
 import { ServerStyleSheet } from "styled-components"
 
 export default class MyDocument extends Document {
@@ -28,5 +28,41 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal()
     }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#28c654" />
+          <meta name="apple-mobile-web-app-title" content="Taskematic" />
+          <meta name="application-name" content="Taskematic" />
+          <meta name="msapplication-TileColor" content="#28c654" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
   }
 }
